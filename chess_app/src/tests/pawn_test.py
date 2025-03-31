7fba0f0cdf3f3fdb6098dd5106f0a2a3e34ca653
Juho Turunen <juho.turunen89@gmail.com>
2025-03-31 21:12:33 +0300

    Added tests for basic pawn moves

---
+++ chess_app/src/tests/pawn_test.py
@@ -0,0 +1,44 @@
+import unittest
+from entities.board import Board
+from services.game_service import GameService
+
+
+class TestGameService(unittest.TestCase):
+    def setUp(self):
+        self.board = Board("white")
+        self.game_service = GameService(self.board)
+        
+    def test_pawn_move(self):
+        self.assertTrue(self.game_service.move_piece((6, 2), (5, 2)))
+
+    def test_pawn_can_jump(self):
+        self.assertTrue(self.game_service.move_piece((6, 2), (4, 2)))
+
+    def test_pawn_loses_jump_ability(self):
+        self.game_service.move_piece((6, 2), (4, 2))
+        self.game_service.move_piece((6, 0), (5, 0))
+
+        self.assertFalse(self.game_service.move_piece((4, 2), (2, 2)))
+        self.assertFalse(self.game_service.move_piece((5, 2), (3, 2)))
+    
+    def test_pawn_can_diagonal_eat(self):
+        self.assertTrue(self.game_service.move_piece((6, 3), (4, 3)))
+        self.assertTrue(self.game_service.move_piece((6, 3), (4, 3)))
+        
+        self.assertTrue(self.game_service.move_piece((4, 3), (3, 4)))
+
+        self.assertEqual(self.game_service.board.get_piece_at((4, 3)), None)
+        self.assertEqual(self.game_service.board.get_piece_at((3, 4)).__repr__(), "wP")
+    
+    def test_pawn_cant_diagonal_move(self):
+        self.assertFalse(self.game_service.move_piece((6, 2), (5, 3)))
+        
+    def test_pawn_cant_forward_eat(self):
+        self.assertTrue(self.game_service.move_piece((6, 4), (4, 4)))
+        self.assertTrue(self.game_service.move_piece((6, 3), (4, 3)))
+
+        self.assertFalse(self.game_service.move_piece((4, 4), (3, 4)))
+
+        self.assertEqual(self.game_service.board.get_piece_at((3, 4)).__repr__(), "bP")
+        self.assertEqual(self.game_service.board.get_piece_at((4, 4)).__repr__(), "wP")
+
