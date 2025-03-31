3477e22bc3a2143d107cd4608fb955deb186908b
Juho Turunen <juho.turunen89@gmail.com>
2025-03-31 21:13:59 +0300

    Added basic invoke commands

--- chess_app/tasks.py
+++ chess_app/tasks.py
@@ -1,1 +1,20 @@
 from invoke import task
+from sys import platform
+
+pty_option = False if platform == "win32" else True
+
+@task
+def start(ctx):
+    ctx.run("python src/main.py", pty=pty_option)
+
+@task
+def test(ctx):
+    ctx.run("pytest", pty=pty_option)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest", pty=pty_option)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=pty_option)
