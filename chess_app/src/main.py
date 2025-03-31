c13a3264fb5eb65c9cf44b16a78014f9b7ce464e
Juho Turunen <juho.turunen89@gmail.com>
2025-03-31 21:14:50 +0300

    Changed main.py simulated game

--- chess_app/src/main.py
+++ chess_app/src/main.py
@@ -5,11 +5,22 @@ def main():
     board = Board("black")
     game_service = GameService(board)
     
-    print(game_service.move_piece((6,2), (5,2)))
-    print(game_service.move_piece((6,2), (5,2)))
-    print(game_service.move_piece((5,2), (4,2)))
-    print(game_service.move_piece((5,2), (4,2)))
+    print(game_service.move_piece((6,2), (4,2)))
+    print(game_service.move_piece((6,0), (5,0)))
+    
     print(game_service.move_piece((4,2), (3,2)))
-    print(game_service.move_piece((4,2), (3,2)))
+    print(game_service.move_piece((5,0), (3,0)))
+    print(game_service.move_piece((5,0), (4,0)))
+    
+    print(game_service.move_piece((3,2), (2,2)))
+    print(game_service.move_piece((4,0), (3,0)))
+    
+    print(game_service.score)
+    
+    print(game_service.move_piece((2,2), (1,2)))
+    print(game_service.move_piece((2,2), (1,3)))
+    
+    print(game_service.score)
+    
     
 main()
