715aca9a0decdb1f06b16e726dc7d95fd11d529a
Juho Turunen <juho.turunen89@gmail.com>
2025-04-22 01:31:35 +0300

    Improved docstrings

--- src/services/game_service.py
+++ src/services/game_service.py
@@ -37,13 +37,13 @@ class GameService:
             self.game_repo = GameRepository()
 
     def move_handler(self, move):
-        """Processes a player move and the corresponding AI response if present.
+        """Processes a player move and the corresponding AI response, if present.
 
         Args:
             move: (start, end) positions as (row, col) tuples.
 
         Returns:
-            Board object or False
+            Board object or False if the move was illegal
         """
         if not self._move_piece(move):
             return False
@@ -69,20 +69,39 @@ class GameService:
         return self.board
 
     def get_game_state(self):
-        """Handles game end states.
+        """Returns the current game state.
 
         Returns:
-            dict of game over state bool and winner (None if not game over)
+            dict {"game_over": bool, "winner": str or None (if game is not over)}
         """
         if self.winner:
             return {"game_over": True, "winner": self.winner}
         return {"game_over": False, "winner": None}
 
-    def _game_end_handler(self, end_state, ai_turn=False):
-        """Handles game end states.
+    def _move_piece(self, move):
+        """Attempts to apply a move to the board, and after, flips the board.
 
         Args:
-            end_state: int
+            move: (start, end) positions as (row, col) tuples.
+
+        Returns:
+            bool (False if move was illegal, else True)
+        """
+        board = simulate_move(self.board, move)
+
+        if not board:
+            return False
+
+        board.flip_board()
+        self.board = board
+
+        return True
+
+    def _game_end_handler(self, end_state, ai_turn=False):
+        """Handles different types of game end states.
+
+        Args:
+            end_state: int (1 for checkmate, 2 for draw)
             ai_turn: bool
         """
         result = None
@@ -104,26 +123,12 @@ class GameService:
         if self.user and self.ai:
             self.game_repo.record_game(self.user.id, result, self.ai.depth)
 
-    def _move_piece(self, move):
-        """Attempts to apply a move and then flips the board.
-
-        Args:
-            move: (start, end) positions as (row, col) tuples.
+    def _is_game_over(self):
+        """Identifies if game is over and how it ended.
 
         Returns:
-            bool
+            int (1 for checkmate, 2 for draw) or False if game has not ended
         """
-        board = simulate_move(self.board, move)
-
-        if not board:
-            return False
-
-        board.flip_board()
-        self.board = board
-
-        return True
-
-    def _is_game_over(self):
         moves = generate_moves(self.board)
 
         for move in moves:
