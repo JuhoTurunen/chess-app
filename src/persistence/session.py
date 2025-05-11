9b91220e5140a4e071ffe28d422d235442c63713
Juho Turunen <juho.turunen89@gmail.com>
2025-05-11 23:52:42 +0300

    Fixed database problem

--- src/persistence/session.py
+++ src/persistence/session.py
@@ -1,14 +1,23 @@
+# pylint: skip-file
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from .models.user import User
+from .models.game import Game
 from .models import Base
 
 DB_PATH = os.getenv("SQLITE_PATH", os.path.join(os.getcwd(), "data", "chess.db"))
 os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
 
-engine = create_engine(f"sqlite:///{DB_PATH}", connect_args={"check_same_thread": False})
+engine = create_engine(
+    f"sqlite:///{DB_PATH}",
+    connect_args={"check_same_thread": False},
+)
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+Base.metadata.create_all(bind=engine)
 
-if not os.path.exists(DB_PATH):
-    Base.metadata.create_all(bind=engine)
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+)
